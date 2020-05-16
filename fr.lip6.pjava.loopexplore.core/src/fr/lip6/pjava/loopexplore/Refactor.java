998e0224ce2ce16ac319e897cb187dd240370bd8
Yann Thierry-Mieg <Yann.Thierry-Mieg@lip6.fr>
2020-05-16 17:00:36 +0200

    separating business logic from ui

--- fr.lip6.pjava.loopexplore.core/src/fr/lip6/pjava/loopexplore/Refactor.java
+++ fr.lip6.pjava.loopexplore.core/src/fr/lip6/pjava/loopexplore/Refactor.java
@@ -1,4 +1,4 @@
-package handler;
+package fr.lip6.pjava.loopexplore;
 
 import java.io.FileWriter;
 import java.io.IOException;
@@ -9,34 +9,14 @@ import java.util.stream.Collectors;
 
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVPrinter;
-import org.eclipse.core.commands.AbstractHandler;
-import org.eclipse.core.commands.ExecutionEvent;
-import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.jdt.core.IJavaProject;
-import org.eclipse.jdt.internal.ui.util.SelectionUtil;
-import org.eclipse.jface.dialogs.MessageDialog;
-import org.eclipse.jface.text.Document;
-import org.eclipse.jface.viewers.ISelection;
-import org.eclipse.ui.IWorkbenchWindow;
-import org.eclipse.ui.handlers.HandlerUtil;
 
 import analyzer.EnhancedForLoopAnalyzer;
 import collector.EnhancedForLoopCollector;
 
+public class Refactor {
 
-
-public class CommandHandler extends AbstractHandler {
-
-	@Override
-	public Object execute(ExecutionEvent event) throws ExecutionException {
-		/* <CUSTOM CODE HERE> */
-		ISelection selection = HandlerUtil.getCurrentSelectionChecked(event);
-		List<?> list = SelectionUtil.toList(selection);
-		List<IJavaProject> javaProjects = list.stream()
-				.filter(e -> e instanceof IJavaProject)
-				.map(e -> (IJavaProject) e)
-				.collect(Collectors.toList());
-
+	public static void refactorProjects(List<IJavaProject> javaProjects) {
 		List<String> resultsHeader = new ArrayList<>(Arrays.asList(
 				"Project", "#SuperFor", "#SubFor", "#Break", "#Continue", "#Return", "#Throw", "#nfnef", "Iterable", "Path", "From", "To", "Refactorable"
 				));
@@ -118,13 +98,6 @@ public class CommandHandler extends AbstractHandler {
 			}
 
 		});
-
-		IWorkbenchWindow window = HandlerUtil.getActiveWorkbenchWindowChecked(event);
-		MessageDialog.openInformation(
-				window.getShell(),
-				"Handler",
-				"Hello, Eclipse world");
-		return null;
 	}
 
 }
---
+++ fr.lip6.pjava.loopexplore.ui/src/handler/CommandHandler.java
@@ -0,0 +1,49 @@
+package handler;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVPrinter;
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.jdt.core.IJavaProject;
+import org.eclipse.jdt.internal.ui.util.SelectionUtil;
+import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.jface.text.Document;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.handlers.HandlerUtil;
+
+import fr.lip6.pjava.loopexplore.Refactor;
+
+
+
+public class CommandHandler extends AbstractHandler {
+
+	@Override
+	public Object execute(ExecutionEvent event) throws ExecutionException {
+		/* <CUSTOM CODE HERE> */
+		ISelection selection = HandlerUtil.getCurrentSelectionChecked(event);
+		List<?> list = SelectionUtil.toList(selection);
+		List<IJavaProject> javaProjects = list.stream()
+				.filter(e -> e instanceof IJavaProject)
+				.map(e -> (IJavaProject) e)
+				.collect(Collectors.toList());
+
+		Refactor.refactorProjects(javaProjects);
+
+		IWorkbenchWindow window = HandlerUtil.getActiveWorkbenchWindowChecked(event);
+		MessageDialog.openInformation(
+				window.getShell(),
+				"Handler",
+				"Hello, Eclipse world");
+		return null;
+	}
+
+}
