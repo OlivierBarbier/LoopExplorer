3d1aa59b695ad13f2e239af4e202b26e669f160e
Yann Thierry-Mieg <Yann.Thierry-Mieg@lip6.fr>
2020-05-16 17:12:43 +0200

    cleanup of the dead unused code

--- fr.lip6.pjava.loopexplore.core/src/collector/EnhancedForLoopCollector.java
+++ fr.lip6.pjava.loopexplore.core/src/collector/EnhancedForLoopCollector.java
@@ -1,12 +1,10 @@
 package collector;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
 import java.util.concurrent.ConcurrentHashMap;
 
-import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.NullProgressMonitor;
 import org.eclipse.jdt.core.ICompilationUnit;
 import org.eclipse.jdt.core.IJavaElement;
@@ -17,7 +15,6 @@ import org.eclipse.jdt.core.ITypeRoot;
 import org.eclipse.jdt.core.JavaModelException;
 import org.eclipse.jdt.core.dom.AST;
 import org.eclipse.jdt.core.dom.ASTParser;
-import org.eclipse.jdt.core.dom.ASTRequestor;
 import org.eclipse.jdt.core.dom.ASTVisitor;
 import org.eclipse.jdt.core.dom.CompilationUnit;
 import org.eclipse.jdt.core.dom.EnhancedForStatement;
@@ -64,17 +61,6 @@ public class EnhancedForLoopCollector extends ASTVisitor {
 									CompilationUnit ccu = (CompilationUnit) parser.createAST(new NullProgressMonitor());
 									parsedCu.add(ccu);
 								}
-
-								// This version was supposed to be more efficient for parsing a group of files
-								// but we can only ask for type resolution of elements within this set of files
-								// So it will now work if your project has any external dependencies.
-//								parser.createASTs(fragment.getCompilationUnits(), new String[0], new ASTRequestor() {
-//									@Override
-//									public void acceptAST(ICompilationUnit source, CompilationUnit ast) {
-//										parsedCu.add(ast);
-//										super.acceptAST(source, ast);
-//									}
-//								}, new NullProgressMonitor());
 							}
 						}
 					}
@@ -117,81 +103,5 @@ public class EnhancedForLoopCollector extends ASTVisitor {
 		CompilationUnit node = (CompilationUnit)parser.createAST(new NullProgressMonitor());
 		return ((TypeDeclaration) node.types().get(0)).resolveBinding();
 	}
-
-	public void collectStreamVersion()
-	{
-		try {
-			Arrays.stream(javaProject.getPackageFragmentRoots())
-			.flatMap((IPackageFragmentRoot root) -> {
-				try {
-					return Arrays.stream(root.getChildren());
-				} catch (JavaModelException e) {
-					// Trick to handle the checked exception
-					throw new RuntimeException(e);
-				}
-			})
-
-			.filter((IJavaElement child) -> child.getElementType() == IJavaElement.PACKAGE_FRAGMENT)
-
-			.map((IJavaElement child) -> (IPackageFragment) child)
-
-			.flatMap((IPackageFragment fragment) -> {
-				try {
-					return Arrays.stream(fragment.getCompilationUnits());
-				} catch (JavaModelException e) {
-					// Trick to handle the checked exception
-					throw new RuntimeException(e);
-				}
-			})
-
-			.map((ICompilationUnit icu) -> {
-				ASTParser parser = ASTParser.newParser(AST.JLS13);
-				parser.setSource(icu);
-				parser.setKind(ASTParser.K_COMPILATION_UNIT);
-				parser.setResolveBindings(true);
-				return (CompilationUnit) parser.createAST(null);
-			})
-
-			.forEach((CompilationUnit cu) -> cu.accept(this))
-			;
-		} catch (JavaModelException e) {
-			e.printStackTrace();
-		}
-	}
-
-
-
-	/**
-	 * Go for a full parse+load into memory, with Binding resolution activated for the set of sources.
-	 * This one pass parse ensures that IBindings resolve to a single instance in memory for a given object
-	 * therefore we can lookup and store IBinding easily in the code (hash, equals, == all OK).
-	 * 
-	 * @param project input Java project carrying important classpath/JLS version etc...
-	 * @param compilationUnits The set of input files we are considering as our scope.
-	 * @param monitor a progress monitor, just forwarded to the parser.
-	 * @return a set of fully resolved and parsed JDT DOM style document model for Java AST. We can use "node.resolveBinding()" in the resulting CompilationUnit.
-	 */
-	public List<CompilationUnit> parseSources(IJavaProject project, ICompilationUnit[] compilationUnits, IProgressMonitor monitor) {
-
-		// Java 8 setting by default, but this setting is overruled by setProject below so irrelevant anyway.
-		ASTParser parser = ASTParser.newParser(AST.JLS8);
-		// parse java files
-		parser.setKind(ASTParser.K_COMPILATION_UNIT);
-		// Important non default setting : resolve name/type bindings for us ! yes, please, thanks !
-		parser.setResolveBindings(true);
-		// actually this overrides a bunch of settings, adds the correct classpath etc... to the underlying ASTresolver/JDT compiler.
-		parser.setProject(project);
-		// Parse the set of input files in one big pass, collect results in parsedCu
-		List<CompilationUnit> parsedCu = new ArrayList<>();
-		parser.createASTs(compilationUnits, new String[0], new ASTRequestor() {
-			@Override
-			public void acceptAST(ICompilationUnit source, CompilationUnit ast) {
-				parsedCu.add(ast);
-				super.acceptAST(source, ast);
-			}
-		}, monitor);
-		return parsedCu;
-	}
-
 
 }
