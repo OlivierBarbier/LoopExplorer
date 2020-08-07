eef67cd399bb51f5d515f8d91292f44cbd3c38b5
Yann Thierry-Mieg <Yann.Thierry-Mieg@lip6.fr>
2020-08-07 17:14:42 +0200

    adopt some features of "parser" branch

---
+++ fr.lip6.pjava.loopexplore.ui/src/fr/lip6/pjava/loopexplore/Refactor.java
@@ -0,0 +1,105 @@
+package fr.lip6.pjava.loopexplore;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVPrinter;
+import org.eclipse.jdt.core.IJavaProject;
+
+import analyzer.EnhancedForLoopAnalyzer;
+import collector.EnhancedForLoopCollector;
+
+public class Refactor {
+
+	public static void refactorProjects(List<IJavaProject> javaProjects) {
+		List<String> resultsHeader = new ArrayList<>(Arrays.asList(
+				"Project", "#SuperFor", "#SubFor", "#Break", "#Continue", "#Return", "#Throw", "#nfnef", "Iterable", "Path", "From", "To", "Refactorable"
+				));
+
+		List<String> refactoringResultsHeader = new ArrayList<>(Arrays.asList(
+				"Original", "Refactoring"
+				));	
+
+		javaProjects.parallelStream().forEach(javaProject -> 
+		{
+			CSVPrinter resultsPrinter = null;
+			CSVPrinter refactoringResultsPrinter = null;
+			try {
+				String folder = javaProject.getProject().getLocation().toFile().getCanonicalPath();
+				resultsPrinter = new CSVPrinter(
+						new FileWriter(folder+"/" + javaProject.getElementName() + ".loop-explorer-results.csv", false),
+						CSVFormat.EXCEL.withHeader(resultsHeader.toArray(new String[resultsHeader.size()]))
+						);
+
+				refactoringResultsPrinter = new CSVPrinter(
+						new FileWriter(folder + "/" + javaProject.getElementName() +".loop-explorer-refacto-results.csv", false),
+						CSVFormat.EXCEL.withHeader(refactoringResultsHeader.toArray(new String[refactoringResultsHeader.size()]))
+						);
+
+			} catch (IOException e1) {			e1.printStackTrace();}
+
+			// AbstractMetricSource metricSource = Dispatcher.getAbstractMetricSource(javaProject);
+			// Metric value = metricSource.getValue(Constants.TLOC);
+			
+			IJavaProject[] javaProjectsarr = new IJavaProject[1];
+			javaProjectsarr[0] = javaProject;
+			EnhancedForLoopCollector eflCollector = new EnhancedForLoopCollector(javaProjectsarr);
+			eflCollector.collect();
+
+			int size = eflCollector.enhancedForStatementSet.size();
+			System.out.println("---");
+			System.out.println("size: "+size);
+			System.out.println("---");
+
+
+			try
+			{
+				List<EnhancedForLoopAnalyzer> eflas = eflCollector.enhancedForStatementSet
+						.stream()
+						//.filter(efla -> efla.getNumberOfBreakStatements() == 0)
+						//.filter(efla -> efla.getNumberOfContinueStatements() == 0)
+						//.filter(efla -> efla.getNumberOfNeitherFinalNorEffectivelyFinalVariables() == 0)
+						//.filter(efla -> efla.getNumberOfReturnStatements() == 0)
+						//.filter(efla -> efla.getNumberOfSubForStatements() == 0)
+						//.filter(efla -> efla.getNumberOfSuperForStatements() == 0)
+						.collect(Collectors.toList());
+				for(EnhancedForLoopAnalyzer efla : eflas)
+				{
+					resultsPrinter.print(efla.getProjectName());
+					resultsPrinter.print(efla.getNumberOfSuperForStatements());
+					resultsPrinter.print(efla.getNumberOfSubForStatements());
+					resultsPrinter.print(efla.getNumberOfBreakStatements());
+					resultsPrinter.print(efla.getNumberOfContinueStatements());
+					resultsPrinter.print(efla.getNumberOfReturnStatements());
+					resultsPrinter.print(efla.getNumberOfThrowStatements());
+					resultsPrinter.print(efla.getNumberOfNeitherFinalNorEffectivelyFinalVariables());
+					resultsPrinter.print(efla.getIterableClassName());
+					resultsPrinter.print(efla.getPath());
+					resultsPrinter.print(efla.getStartLine());
+					resultsPrinter.print(efla.getEndLine());
+					resultsPrinter.print(efla.isRefactorable() ? "Yes" : "No");
+					resultsPrinter.println();
+
+					if (efla.isRefactorable()) {
+						refactoringResultsPrinter.print(efla.efs.toString());
+						refactoringResultsPrinter.print(efla.getRefactoring());
+						refactoringResultsPrinter.println();
+					}
+				}
+				resultsPrinter.close();
+				refactoringResultsPrinter.close();
+			}
+			catch (IOException e1)
+			{
+				e1.printStackTrace();
+			}
+
+		});
+	}
+
+}
--- fr.lip6.pjava.loopexplore.ui/src/handler/CommandHandler.java
+++ fr.lip6.pjava.loopexplore.ui/src/handler/CommandHandler.java
@@ -1,27 +1,19 @@
 package handler;
 
-import java.io.FileWriter;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
-import org.apache.commons.csv.CSVFormat;
-import org.apache.commons.csv.CSVPrinter;
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.jdt.core.IJavaProject;
 import org.eclipse.jdt.internal.ui.util.SelectionUtil;
 import org.eclipse.jface.dialogs.MessageDialog;
-import org.eclipse.jface.text.Document;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.ui.handlers.HandlerUtil;
 
-import analyzer.EnhancedForLoopAnalyzer;
-import collector.EnhancedForLoopCollector;
+import fr.lip6.pjava.loopexplore.Refactor;
 
 
 
@@ -32,87 +24,13 @@ public class CommandHandler extends AbstractHandler {
 		/* <CUSTOM CODE HERE> */
 		ISelection selection = HandlerUtil.getCurrentSelectionChecked(event);
 		List<?> list = SelectionUtil.toList(selection);
-		IJavaProject[] javaProjects = list.stream()
+		List<IJavaProject> javaProjects = list.stream()
 				.filter(e -> e instanceof IJavaProject)
-				.toArray(length -> new IJavaProject[length]);
-	
-		List<String> resultsHeader = new ArrayList<>(Arrays.asList(
-				"Project", "#SuperFor", "#SubFor", "#Break", "#Continue", "#Return", "#Throw", "#nfnef", "Iterable", "Path", "From", "To", "Refactorable"
-			));
-		
-		List<String> refactoringResultsHeader = new ArrayList<>(Arrays.asList(
-				"Original", "Refactoring"
-			));	
-		
-		CSVPrinter resultsPrinter = null;
-		CSVPrinter refactoringResultsPrinter = null;
-		try {
-			resultsPrinter = new CSVPrinter(
-					new FileWriter("loop-explorer-results.csv", false),
-					CSVFormat.EXCEL.withHeader(resultsHeader.toArray(new String[resultsHeader.size()]))
-			);
-			
-			refactoringResultsPrinter = new CSVPrinter(
-					new FileWriter("loop-explorer-refacto-results.csv", false),
-					CSVFormat.EXCEL.withHeader(refactoringResultsHeader.toArray(new String[refactoringResultsHeader.size()]))
-			);
-			
-		} catch (IOException e1) {			e1.printStackTrace();}
-		
-		// AbstractMetricSource metricSource = Dispatcher.getAbstractMetricSource(javaProject);
-		// Metric value = metricSource.getValue(Constants.TLOC);
-				
-		EnhancedForLoopCollector eflCollector = new EnhancedForLoopCollector(javaProjects);
-		eflCollector.collect();
-		
-		int size = eflCollector.enhancedForStatementSet.size();
-		System.out.println("---");
-		System.out.println("size: "+size);
-		System.out.println("---");
-
-		
-		try
-		{
-			List<EnhancedForLoopAnalyzer> eflas = eflCollector.enhancedForStatementSet
-				.stream()
-				//.filter(efla -> efla.getNumberOfBreakStatements() == 0)
-				//.filter(efla -> efla.getNumberOfContinueStatements() == 0)
-				//.filter(efla -> efla.getNumberOfNeitherFinalNorEffectivelyFinalVariables() == 0)
-				//.filter(efla -> efla.getNumberOfReturnStatements() == 0)
-				//.filter(efla -> efla.getNumberOfSubForStatements() == 0)
-				//.filter(efla -> efla.getNumberOfSuperForStatements() == 0)
+				.map(e -> (IJavaProject) e)
 				.collect(Collectors.toList());
-			for(EnhancedForLoopAnalyzer efla : eflas)
-			{
-				resultsPrinter.print(efla.getProjectName());
-				resultsPrinter.print(efla.getNumberOfSuperForStatements());
-				resultsPrinter.print(efla.getNumberOfSubForStatements());
-				resultsPrinter.print(efla.getNumberOfBreakStatements());
-				resultsPrinter.print(efla.getNumberOfContinueStatements());
-				resultsPrinter.print(efla.getNumberOfReturnStatements());
-				resultsPrinter.print(efla.getNumberOfThrowStatements());
-				resultsPrinter.print(efla.getNumberOfNeitherFinalNorEffectivelyFinalVariables());
-				resultsPrinter.print(efla.getIterableClassName());
-				resultsPrinter.print(efla.getPath());
-				resultsPrinter.print(efla.getStartLine());
-				resultsPrinter.print(efla.getEndLine());
-				resultsPrinter.print(efla.isRefactorable() ? "Yes" : "No");
-				resultsPrinter.println();
-				
-				if (efla.isRefactorable()) {
-					refactoringResultsPrinter.print(efla.efs.toString());
-					refactoringResultsPrinter.print(efla.getRefactoring());
-					refactoringResultsPrinter.println();
-				}
-			}
-			resultsPrinter.close();
-			refactoringResultsPrinter.close();
-		}
-		catch (IOException e1)
-		{
-			e1.printStackTrace();
-		}
-		
+
+		Refactor.refactorProjects(javaProjects);
+
 		IWorkbenchWindow window = HandlerUtil.getActiveWorkbenchWindowChecked(event);
 		MessageDialog.openInformation(
 				window.getShell(),
